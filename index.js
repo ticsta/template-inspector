2abba84e3fe9d319b83039de56c22ea647959139
Dumitru Cantea <canteadumitru@gmail.com>
2015-04-09 16:15:28 +0300

    Validating template

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var Validator = require('./lib/validator');
+var TemplateLoader = require('./lib/template_loader');
 
 module.exports.Validator = Validator;
+module.exports.templateLoader = TemplateLoader;
