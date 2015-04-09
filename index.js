61e145b7c0cca0efaba0d5e98ee84f227f05715b
Dumitru Cantea <canteadumitru@gmail.com>
2015-04-09 19:46:48 +0300

    version 0.0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Validator = require('./lib/validator');
 var TemplateLoader = require('./lib/template_loader');
 
-module.exports.Validator = Validator;
+module.exports.validator = Validator;
 module.exports.templateLoader = TemplateLoader;
