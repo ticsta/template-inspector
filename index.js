6cf52ddf4cc0ce4a1473792cca5205b7988529dd
Dumitru Cantea <canteadumitru@gmail.com>
2015-04-11 17:06:20 +0300

    renamed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Validator = require('./lib/validator');
 var TemplateLoader = require('./lib/template_loader');
 
-module.exports.validator = Validator;
-module.exports.templateLoader = TemplateLoader;
+module.exports.validate = Validator.validate;
+module.exports.load = TemplateLoader.load;
